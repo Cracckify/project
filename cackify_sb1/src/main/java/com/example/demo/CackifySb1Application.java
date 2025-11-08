15054fe6418efb97e4c0f0000073aff31be2d6ea
venkat-gottemukkala <se.subhas@gmail.com>
2025-11-08 13:40:42 +0530

    added one project

---
+++ cackify_sb1/src/main/java/com/example/demo/CackifySb1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CackifySb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CackifySb1Application.class, args);
+		
+		
+		System.out.println(" first spring boot app");
+	}
+
+}
