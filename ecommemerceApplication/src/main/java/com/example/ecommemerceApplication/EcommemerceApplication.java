645bc1230e013b70f9e2f7100a204c54850d848b
Ranyr <104067590+Ranyr@users.noreply.github.com>
2025-11-08 13:42:43 +0530

    Add files via upload
    project uploaded

---
+++ ecommemerceApplication/src/main/java/com/example/ecommemerceApplication/EcommemerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommemerceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommemerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommemerceApplication.class, args);
+	}
+
+}
---
+++ ecommemerceApplication/src/test/java/com/example/ecommemerceApplication/EcommemerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommemerceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommemerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
