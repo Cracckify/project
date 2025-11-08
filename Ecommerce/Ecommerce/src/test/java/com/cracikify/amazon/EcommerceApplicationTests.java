48c63d40fedf73de61997dc5d9a5caaebfca6bd3
Rishabh3298 <157232137+Rishabh3298@users.noreply.github.com>
2025-11-08 13:48:58 +0530

    Add files via upload

---
+++ Ecommerce/Ecommerce/src/test/java/com/cracikify/amazon/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cracikify.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
