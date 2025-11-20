95e291388e5a67e4546ec9a10dbc05192ad66828
Joel Johny <joeljohny11j@gmail.com>
2025-11-20 10:45:28 +0530

    Added sout log

--- amazon/src/main/java/com/example/amazon/AmazonApplication.java
+++ amazon/src/main/java/com/example/amazon/AmazonApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmazonApplication {
 
 	public static void main(String[] args) {
+        System.out.println("setup completed");
 		SpringApplication.run(AmazonApplication.class, args);
+
 	}
 
 }
